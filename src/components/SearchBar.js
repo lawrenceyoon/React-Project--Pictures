9ab37b66b265dcd624b58c6c7c4207c5dccaf605
Lawrence <yoonlawrence@yahoo.com>
2020-02-12 05:39:11 -0800

    Checking heroku

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -19,7 +19,7 @@ const SearchBar = () => {
       <div className="ui segment">
         <form className="ui form" onSubmit={onFormSubmit}>
           <div className="field">
-            <label>Image Search: </label>
+            <label>Image Searchasdfasdf: </label>
             <input type="text" value={searchTerm} onChange={onInputChange} placeholder="Search..." />
           </div>
         </form>
